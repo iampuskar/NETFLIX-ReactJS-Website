4c0bf9de9dbfaba8f8d76acb555d6df00d44ed52
PuskarGhimire <puskar93@gmail.com>
2021-11-18 13:31:35 +0545

    Project added with Navbar component Ready

---
+++ src/home/Home.jsx
@@ -0,0 +1,27 @@
+import Navbar from "../components/navbar/Navbar"
+import "./home.scss"
+
+function Home() {
+    return (
+        <div className="home">
+            <Navbar/>
+            <img width="100%" src="https://images.pexels.com/photos/1182127/pexels-photo-1182127.jpeg?cs=srgb&dl=pexels-ba-phi-1182127.jpg&fm=jpg" alt="" />
+            
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias adipisci illo est suscipit. Voluptate cum quidem aliquam itaque laudantium culpa vitae iure minus rerum inventore odio quaerat nihil, sit vero.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias adipisci illo est suscipit. Voluptate cum quidem aliquam itaque laudantium culpa vitae iure minus rerum inventore odio quaerat nihil, sit vero.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias adipisci illo est suscipit. Voluptate cum quidem aliquam itaque laudantium culpa vitae iure minus rerum inventore odio quaerat nihil, sit vero.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias adipisci illo est suscipit. Voluptate cum quidem aliquam itaque laudantium culpa vitae iure minus rerum inventore odio quaerat nihil, sit vero.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias adipisci illo est suscipit. Voluptate cum quidem aliquam itaque laudantium culpa vitae iure minus rerum inventore odio quaerat nihil, sit vero.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias adipisci illo est suscipit. Voluptate cum quidem aliquam itaque laudantium culpa vitae iure minus rerum inventore odio quaerat nihil, sit vero.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias adipisci illo est suscipit. Voluptate cum quidem aliquam itaque laudantium culpa vitae iure minus rerum inventore odio quaerat nihil, sit vero.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias adipisci illo est suscipit. Voluptate cum quidem aliquam itaque laudantium culpa vitae iure minus rerum inventore odio quaerat nihil, sit vero.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias adipisci illo est suscipit. Voluptate cum quidem aliquam itaque laudantium culpa vitae iure minus rerum inventore odio quaerat nihil, sit vero.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias adipisci illo est suscipit. Voluptate cum quidem aliquam itaque laudantium culpa vitae iure minus rerum inventore odio quaerat nihil, sit vero.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias adipisci illo est suscipit. Voluptate cum quidem aliquam itaque laudantium culpa vitae iure minus rerum inventore odio quaerat nihil, sit vero.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias adipisci illo est suscipit. Voluptate cum quidem aliquam itaque laudantium culpa vitae iure minus rerum inventore odio quaerat nihil, sit vero.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias adipisci illo est suscipit. Voluptate cum quidem aliquam itaque laudantium culpa vitae iure minus rerum inventore odio quaerat nihil, sit vero.
+        </div>
+    )
+}
+
+export default Home
